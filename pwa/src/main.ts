cb2074e0ee5a391ab0c34aeb5de04dd8598eafde
Jonny Wyatt <jonnywyatt@yahoo.co.uk>
2026-04-08 20:38:26 +0100

    Fix stale SW cache routing issue

--- pwa/src/main.ts
+++ pwa/src/main.ts
@@ -3,6 +3,12 @@ import './styles/main.css';
 import App from './App.vue';
 import router from './router';
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.addEventListener('controllerchange', () => {
+    window.location.reload();
+  });
+}
+
 const app = createApp(App);
 app.use(router);
 app.mount('#app');
